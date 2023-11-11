73aa4244897706beff941e48d12a3d0e6ba097b5
nilsyadv <nilesh.coherent@gmail.com>
2023-11-11 16:46:33 +0530

    customer setup done

--- customer/cmd/main.go
+++ customer/cmd/main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	"github.com/nilsyadv/ShopBillBuddy/common/pkg/logger"
+	"github.com/nilsyadv/ShopBillBuddy/customer/config"
+)
+
+func main() {
+	conf, werr := config.InitConf("config", "json")
+	if werr != nil {
+		log.Fatal(werr.Error())
+	}
+
+	custlog, err := logger.InitLogger(conf.GetString("logger.level"), conf.GetString("logger.output"))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	custlog.Info("Customer server started")
+}
