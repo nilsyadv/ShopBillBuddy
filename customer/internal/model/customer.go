4b4ee982e9a1908e8291868384aefd99b1e9d19f
nilsyadv <nilesh.coherent@gmail.com>
2023-11-13 15:13:17 +0530

    customer struct added

--- customer/internal/model/customer.go
+++ customer/internal/model/customer.go
@@ -1,5 +1,23 @@
 package model
 
 type Customer struct {
+	ID      string
+	Name    string
+	Remark  string
+	Contact Contact
+	Address Address
+}
+
+type Contact struct {
+	MobileNo    string
+	Email       string
+	AlternateNo string
+}
+
+type Address struct {
+	ID         string
+	Address    string
+	Zipcode    string
+	Landmark   string
 	CustomerID string
 }
